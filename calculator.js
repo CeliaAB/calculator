b1a035ead30f117fdc3e093a928646b4cd95f87a
Celia Bailey <sarah.bacen@gmail.com>
2019-05-19 20:24:54 +1200

    Update calculator first draft

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+// take an operand or operator, store it 
+// if "=" is entered, display the product and replace the product in the working area - not sure if this should be kept for next calc
+// if C is pressed, clear the last entry
+// if AC is pressed, reset the calculator
 document.addEventListener('DOMContentLoaded', startCalculator)
 
 // set the total to zero and creates an array called entries
@@ -7,28 +11,78 @@ var total = 0;
 //set a temp workspace
 var temp = '';
 
-// take a number entry (operand), store it, so need to determine if input is a number (NaN)
-$("button").on('click', function() {
+//set the display
+let display = document.getElementById("workings");
 
-// set var val to the number thats entered
-    var val = $(this).text();
-// test whether its a number (does sequence of input matter - i.e. if "." is b4 or after #)
-    if (!isNaN(val) || val === '.') {
-        temp += val;
-        $("#answer").val(temp.substring(0,10));
+//notes: I need clear all field if AC is clicked
+//notes: I need a place to store the product if say ** or ÷÷ is clicked maybe "err" for the second?
+//notes: I need a place to store the product if C is clicked to show what is currently solution
+//notes: I need to display product if = is clicked to show what is currently solution
+//notes: I need a decide what to show and logic if % is clicked
+//notes: Need to change sign if +/- is clicked
+//notes: Need to make sure 2 decimals are not clicked maybe "err" for the second?
+//notes: maybe keep a copy of the display so that when C is clicked its not appended to the display string
 
-// take an operator, store it (maybe into a string?)
-// take the last part of the equation (could be a operand or an operator), store it (maybe into a string?)
-// when the next entry is received, work out the product of the first 3 entries
-// if the fourth entry is an =, display the product and replace the product in the string (like the first entry)
-// if the fourth entry is + - * /, add to string and repeat until all clear is pressed
-// if c is pressed, clear the last entry
+function startCalculator () {
+    document.addEventListener('click', keyedInput);
+}
 
-// Push number
-//} else {
-    entries.push(temp);
-    entries.push(val);
-    temp = '';
+//get the input and check if its an operand or an operator
+function keyedInput () {
+    document.addEventListener('click', workings)
+    var inputButton = event.target;
+    var getValue = inputButton.value;
+    //test if the input is a number by using built in Number()
+    var checkIfNum = Number(getValue);
+
+    /*//clear last entry if the clear button has been hit (AC has been cleared in html)
+    if (getValue === "C") {
+        if (temp !=='') {
+            display.value = temp;
+    //reset the array
+    //        function empty() {
+    //            entries = []
+    //        };
+        }
     }
-})
+    */
+    //if the input is a number or a decimal add to temp
+    if (checkIfNum != NaN || getValue === ".") {
+        temp += getValue;
+        display.value = temp;
+    } 
+//something wrong with the totals
+    //if the input is an "=" get the product by working through entries
+    if (getValue === "=") {
+        
+        entries.push(temp);
+        //the arithmetic is to start with the first operand, then use operator with the next and so on    
+        //as there are number entries and operators, need to test for each
+        var product = Number(entries[0]);
+        for (var i = 1; i < entries.length; i ++) {
+            var nextNum = Number(entries[i+1]);
+            var operator = entries[i];
 
+            //work through the array, in order because calculators aren't honouring operator priority
+            if (operator === "+") {
+                product += nextNum;
+            }
+            else if (operator === "-") {
+                product -= nextNum;
+            }
+            else if (operator === "*") {
+                product *= nextNum;
+            }
+            else if (operator === "/") {
+                product /= nextNum;
+            }
+            else if (operator === "%") {
+                product %= nextNum;
+            }
+        }
+        display.value = product;
+        //reset work area
+        //entries = [];
+        //temp = '';
+    }
+}
