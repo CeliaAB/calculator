14cc77299508d6bb9593c07766e9bb2e74a38888
Celia Bailey <sarah.bacen@gmail.com>
2019-05-20 19:38:10 +1200

    Update calculator css to rid bootstrap

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 // take an operand or operator, store it 
 // if "=" is entered, display the product and replace the product in the working area - not sure if this should be kept for next calc
 // if C is pressed, clear the last entry
-// if AC is pressed, reset the calculator
+
 document.addEventListener('DOMContentLoaded', startCalculator)
 
 // set the total to zero and creates an array called entries
@@ -10,11 +10,12 @@ var total = 0;
 
 //set a temp workspace
 var temp = '';
+var lastEntry = '';
 
 //set the display
 let display = document.getElementById("workings");
 
-//notes: I need clear all field if AC is clicked
+//notes: Need to sort out if +- is clicked, in that order
 //notes: I need a place to store the product if say ** or ÷÷ is clicked maybe "err" for the second?
 //notes: I need a place to store the product if C is clicked to show what is currently solution
 //notes: I need to display product if = is clicked to show what is currently solution
@@ -32,32 +33,32 @@ function keyedInput () {
     document.addEventListener('click', workings)
     var inputButton = event.target;
     var getValue = inputButton.value;
+
     //test if the input is a number by using built in Number()
     var checkIfNum = Number(getValue);
 
-    /*//clear last entry if the clear button has been hit (AC has been cleared in html)
-    if (getValue === "C") {
-        if (temp !=='') {
-            display.value = temp;
-    //reset the array
-    //        function empty() {
-    //            entries = []
-    //        };
-        }
-    }
-    */
     //if the input is a number or a decimal add to temp
     if (checkIfNum != NaN || getValue === ".") {
         temp += getValue;
         display.value = temp;
+        entries.push(temp);
     } 
-//something wrong with the totals
+
+    //if everytime a valid entry is hit, temp is pushed to entries, then getting rid
+    //of the last entry if "C" is hit means the correct string is now the last entry
+    
+    //call a function to delete the last entry when "C" is hit (AC has been cleared in html)
+    if (getValue === "C") {
+            entries.pop();
+            temp = entries[entries.length - 1];
+            display.value = temp;
+    }
+
     //if the input is an "=" get the product by working through entries
     if (getValue === "=") {
-        
-        entries.push(temp);
         //the arithmetic is to start with the first operand, then use operator with the next and so on    
         //as there are number entries and operators, need to test for each
+        var lastEntry = entries[entries.length -1];
         var product = Number(entries[0]);
         for (var i = 1; i < entries.length; i ++) {
             var nextNum = Number(entries[i+1]);
